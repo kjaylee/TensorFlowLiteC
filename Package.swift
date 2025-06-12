f5d869060bb67d22c321ad67ad08f714ce37adbd
hjlee <hjlee@aiforpet.com>
2025-06-12 16:11:24 +0900

    feat: 직접 참조로 수정.

--- Package.swift
+++ Package.swift
@@ -1,24 +1,27 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.1
+// tag: "1.1.0"
 
 import PackageDescription
 
 let package = Package(
     name: "TensorFlowLiteC",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "TensorFlowLiteC",
             targets: [
                 "TensorFlowLiteC",
-                "TensorFlowLiteCCoreML",
-                "TensorFlowLiteCMetal",
                 "Link",
             ]
         ),
     ],
     targets: [
-        .binaryTarget(name: "TensorFlowLiteC", url: "https://github.com/kewlbear/TensorFlowLiteC/releases/download/0.0.20250506/TensorFlowLiteC.xcframework.zip", checksum: "cf412e9dec2f33d523f6ddd2bb312a7fd360eb04e1ffea9ebf8c7f414b36268b"),
-        .binaryTarget(name: "TensorFlowLiteCCoreML", url: "https://github.com/kewlbear/TensorFlowLiteC/releases/download/0.0.20250506/TensorFlowLiteCCoreML.xcframework.zip", checksum: "0f3b92574fd8236443145d8c27a9ca98b4cd368cd70460df7a8f0efd74749b83"),
-        .binaryTarget(name: "TensorFlowLiteCMetal", url: "https://github.com/kewlbear/TensorFlowLiteC/releases/download/0.0.20250506/TensorFlowLiteCMetal.xcframework.zip", checksum: "00c36052ede6d3583302842bf15323985a7eac9fa87c02725016a7744d6ecf51"),
+        .binaryTarget(
+            name: "TensorFlowLiteC",
+            path: "TensorFlowLiteC.xcframework"
+        ),
         .target(name: "Link",
                 linkerSettings: [.linkedLibrary("c++")]),
     ]
